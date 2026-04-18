3b52eb4c81d3639d5782aa2b116c11336fe9e52f
zumar-fatima <zumar.fatima@tycoonzsolutions.com>
2026-04-18 11:28:42 +0500

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  allowedDevHosts: ["underfoot-dose-vexingly.ngrok-free.app"],
 };
 
 export default nextConfig;
